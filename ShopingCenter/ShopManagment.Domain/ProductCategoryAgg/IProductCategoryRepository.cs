5e2933a7ad7f83d49f2c98f93cbb9ec75c724a2d
meghlimapour <meghlimapour@HA-94BL185>
2022-04-12 12:51:42 +0430

    create solutionSM domain and application create product category model and view model create product category repository create framework solution for base entity reapeted

---
+++ ShopingCenter/ShopManagment.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+        ProductCategory Get(int id);
+        List<ProductCategory> GetAll();
+    }
+}
