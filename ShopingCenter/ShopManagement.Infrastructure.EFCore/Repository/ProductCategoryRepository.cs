101dacf587478ceeed32744488298db78eafdc89
meghlimapour <meghlimapour@HA-94BL185>
2022-04-12 15:37:02 +0430

    create context and repository product category

---
+++ ShopingCenter/ShopManagement.Infrastructure.EFCore/Repository/ProductCategoryRepository.cs
@@ -0,0 +1,76 @@
+﻿using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ShopManagement.Infrastructure.EFCore.Repository
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private readonly ShopContext _context;
+
+        public ProductCategoryRepository(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(ProductCategory entity)
+        {
+            _context.productCategories.Add(entity);
+        }
+
+        public bool Exists(Expression<Func<ProductCategory, bool>> expression)
+        {
+            return _context.productCategories.Any(expression);
+        }
+
+        public ProductCategory Get(long id)
+        {
+            return _context.productCategories.Find(id);
+        }
+
+        public List<ProductCategory> GetAll()
+        {
+            return _context.productCategories.ToList();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _context.productCategories.Select(x => new EditProductCategory()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                Slug = x.Slug,
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            var query = _context.productCategories.Select(x => new ProductCategoryViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                CreationDate = x.CreationDate.ToString(),
+                Picture = x.Picture,
+            });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+
+            return query.OrderByDescending(x => x.Id).ToList();
+        }
+    }
+}
---
+++ ShopingCenter/ShopManagement.Infrastructure.EFCore/ShopContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EFCore.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Infrastructure.EFCore
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<ProductCategory> productCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(ProductCategoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
