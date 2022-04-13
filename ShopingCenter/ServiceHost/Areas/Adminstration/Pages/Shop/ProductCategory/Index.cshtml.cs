50425d389e5171ec013f8e6defd0f18daaaf2980
meghlimapour <meghlimapour@HA-94BL185>
2022-04-13 16:59:42 +0430

    add create product category change or add css site and jquery

--- ShopingCenter/ServiceHost/Areas/Adminstration/Pages/Shop/ProductCategory/Index.cshtml.cs
+++ ShopingCenter/ServiceHost/Areas/Adminstration/Pages/Shop/ProductCategory/Index.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.ProductCategory;
+using System.Collections.Generic;
 
 namespace ServiceHost.Areas.Adminstration.Pages.Shop.ProductCategory
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public ProductCategorySearchModel SearchModel;
+        public List<ProductCategoryViewModel> productCategories;
+        private readonly IProductCategoryApplication _productCategoryApplication;
+
+        public IndexModel(IProductCategoryApplication productCategoryApplication)
         {
+            _productCategoryApplication = productCategoryApplication;
+        }
+
+        public void OnGet(ProductCategorySearchModel searchModel)
+        {
+            productCategories = _productCategoryApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            return Partial("./Create", new CreateProductCategory());
+        }
+
+        public JsonResult OnPostCreate(CreateProductCategory command)
+        {
+            var resualt = _productCategoryApplication.Create(command);
+            return new JsonResult(resualt);
         }
     }
 }
