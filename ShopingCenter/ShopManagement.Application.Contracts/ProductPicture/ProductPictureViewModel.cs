374b88e7efef538596d321c0b64fcda39c4fa932
meghlimapour <meghlimapour@HA-94BL185>
2022-04-17 12:41:35 +0430

    implement product picture agg - add repository - add application -add contract

---
+++ ShopingCenter/ShopManagement.Application.Contracts/ProductPicture/ProductPictureViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagement.Application.Contracts.ProductPicture
+{
+    public class ProductPictureViewModel
+    {
+        private long Id { get; set; }
+        public string Product { get; private set; }
+        public string Picture { get; private set; }
+        public string CreationDate { get; set; }
+    }
+}
