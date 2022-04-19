83668bcab2d773c1433137a5440b4ff2ce65d7c6
meghlimapour <meghlimapour@HA-94BL185>
2022-04-19 15:56:05 +0430

    add link to slide model add component slide add Query solution for components Slider in UI impelemented

--- ShopingCenter/ShopManagment.Domain/SlideAgg/Slide.cs
+++ ShopingCenter/ShopManagment.Domain/SlideAgg/Slide.cs
@@ -11,9 +11,10 @@ namespace ShopManagement.Domain.SlideAgg
         public string Title { get; private set; }
         public string Text { get; private set; }
         public string BtnText { get; private set; }
+        public string Link { get; set; }
         public bool IsRemoved { get; set; }
 
-        public Slide(string picture, string pictureAlt, string pictureTitle, string heading, string title, string text, string btnText)
+        public Slide(string picture, string pictureAlt, string pictureTitle, string heading, string title, string text, string btnText, string link)
         {
             Picture = picture;
             PictureAlt = pictureAlt;
@@ -23,9 +24,10 @@ namespace ShopManagement.Domain.SlideAgg
             Text = text;
             BtnText = btnText;
             IsRemoved = false;
+            Link = link;
         }
 
-        public void Edit(string picture, string pictureAlt, string pictureTitle, string heading, string title, string text, string btnText)
+        public void Edit(string picture, string pictureAlt, string pictureTitle, string heading, string title, string text, string btnText, string link)
         {
             Picture = picture;
             PictureAlt = pictureAlt;
@@ -34,6 +36,7 @@ namespace ShopManagement.Domain.SlideAgg
             Title = title;
             Text = text;
             BtnText = btnText;
+            Link = link;
         }
 
         public void Remove()
