58d2a2ea5091a8ea42ab46d5ef6be0ead4598a98
MiladDev <milad.eghlimapour@gmail.com>
2022-08-12 01:16:06 +0430

    test

---
+++ ShopingCenter/DiscountManagement.Configuration/DiscountManagmentBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using DiscountManagement.Application;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using DiscountManagement.Infrastracture.EFCore;
+using DiscountManagement.Infrastracture.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DiscountManagement.Configuration
+{
+    public class DiscountManagmentBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<ICustomerDiscountApplication, CustomerDiscountApplication>();
+            services.AddTransient<ICustomerDiscountRepository, CustomerDiscountRepository>();
+
+            services.AddDbContext<DiscountContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
---
+++ ShopingCenter/DiscountManagement.Infrastracture.EFCore/Repository/CustomerDiscountRepository.cs
@@ -0,0 +1,73 @@
+﻿using _0_Framework.Application;
+using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using ShopManagement.Infrastructure.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DiscountManagement.Infrastracture.EFCore.Repository
+{
+    public class CustomerDiscountRepository : RepositoryBase<long, CustomerDiscount>, ICustomerDiscountRepository
+    {
+        private readonly DiscountContext _context;
+        private readonly ShopContext _shopContext;
+
+        public CustomerDiscountRepository(DiscountContext context, ShopContext shopContext) : base(context)
+        {
+            _context = context;
+            _shopContext = shopContext;
+        }
+
+        public EditCustomerDiscount GetDetails(long id)
+        {
+            return _context.customerDiscounts.Select(x => new EditCustomerDiscount
+            {
+                Id = id,
+                DiscountRate = x.DiscountRate,
+                ProductId = x.ProductId,
+                Reason = x.Reason,
+                EndDate = x.EndDate.ToFarsi(),
+                StartDate = x.StartDate.ToFarsi(),
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
+        {
+            var products = _shopContext.Products.Select(x => new { x.Id, x.Name }).ToList();
+
+            var query = _context.customerDiscounts.Select(x => new CustomerDiscountViewModel
+            {
+                Id = x.Id,
+                DiscountRate = x.DiscountRate,
+                ProductId = x.ProductId,
+                Reason = x.Reason,
+                StartDate = x.StartDate.ToFarsi(),
+                EndDate = x.EndDate.ToFarsi(),
+                StartDateGr = x.StartDate,
+                EndDateGr = x.EndDate,
+            });
+
+            if (searchModel.ProductId > 0)
+                query = query.Where(x => x.ProductId == searchModel.ProductId);
+
+            if (!string.IsNullOrWhiteSpace(searchModel.StartDate))
+            {
+                query = query.Where(x => x.StartDateGr > searchModel.StartDate.ToGeorgianDateTime());
+            }
+            if (!string.IsNullOrWhiteSpace(searchModel.EndDate))
+            {
+                query = query.Where(x => x.EndDateGr < searchModel.EndDate.ToGeorgianDateTime());
+            }
+
+            var discounts = query.OrderByDescending(x => x.Id).ToList();
+
+            discounts.ForEach(discount =>
+                discount.Product = products.FirstOrDefault(x => x.Id == discount.ProductId)?.Name);
+
+            return discounts;
+        }
+    }
+}
